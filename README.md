24b2490499be868c9523432461408dc19a4b2531
Raz <47579882+razcodes@users.noreply.github.com>
2019-09-19 15:39:56 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+#### Steps required to successfully run the application
+
+Run the `index.html` file with your browser in order to load the website locally.
+
+#### What did I learn?
+
+I have learned how to use Jasmine to write a number of tests against a pre-existing application. 
+These test the underlying business logic of the application as well as the event handling and DOM manipulation.
+
+#### Acknowledgements
+All files except `feedreader.js` were written and provided by Udacity.
+
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,108 @@
+$(function() {
+    /* A test that makes sure our allFeeds array is
+    *  well defined and not empty.
+    */
+    describe('RSS Feeds', function() {
+
+        it('feeds are defined', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+
+        /* A test that loops through each feed
+         * in the allFeeds object and ensures it has a URL defined
+         * and that the URL is not empty.
+         */
+
+        it('feed url is defined', () => {
+            for(let i = 0; i < allFeeds.length; i++){
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url).not.toBeNull();
+                expect(allFeeds[i].url.length).not.toBe(0);  
+            }
+         });
+
+        /* A test that loops through each feed
+         * in the allFeeds object and ensures it has a name defined
+         * and that the name is not empty.
+         */
+
+         it('feed name is defined', () => {
+            for(let i = 0; i < allFeeds.length; i++){
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name).not.toBeNull();
+                expect(allFeeds[i].name.length).not.toBe(0);
+            }
+         });
+    });
+
+    describe('The menu', () => {
+
+        const bodyElement = document.querySelector("body");
+        const menuButton = document.getElementsByClassName("icon-list")[0];
+
+        /* A test that ensures the menu element is
+         * hidden by default.
+         */
+
+         it('should be hidden', () => {
+            expect(bodyElement.classList.contains("menu-hidden")).toBe(true);
+         });
+
+         /* A test that ensures the menu changes
+          * visibility when the menu icon is clicked.
+          */
+
+          it('should toggle visibility', () => {
+            menuButton.click();
+            expect(bodyElement.classList.contains("menu-hidden")).toBe(false);
+
+            menuButton.click();
+            expect(bodyElement.classList.contains("menu-hidden")).toBe(true);
+          });
+    });
+
+        /* A test that ensures when the loadFeed
+         * function is called and completes its work, there is at least
+         * a single .entry element within the .feed container.
+         */
+    describe('Initial Entries', () => {
+
+        const feed = document.getElementsByClassName("feed")[0];
+
+
+        beforeEach( (done) => {
+            loadFeed(0, () => {
+                done();
+            });
+        });
+
+        it('loads feed properly', () => {
+            expect(feed.childNodes.length).toBeGreaterThan(0);
+        });
+    });
+
+        /* A test that ensures when a new feed is loaded
+         * by the loadFeed function the content actually changes.
+         */
+
+    describe("New Feed Selection", () => {
+        
+        let firstArticle;
+        let secondArticle;
+
+        beforeEach( (done) => {
+            loadFeed(0, () => {
+                firstArticle = document.querySelector('.entry-link').getAttribute("href");
+            });
+            loadFeed(1, () => {
+                secondArticle = document.querySelector('.entry-link').getAttribute("href");
+                done();
+            });
+        });
+
+        it('content changes at new feed load', () => {  
+            expect(firstArticle).not.toEqual(secondArticle);
+        });
+    });
+}());
